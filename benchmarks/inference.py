7b662d818c2d23430c58220216ff4835b88c9fb0
Ankur Ankan <ankurankan@gmail.com>
2022-05-23 11:08:37 +0200

    Change timeout for belief propagation

--- benchmarks/inference.py
+++ benchmarks/inference.py
@@ -17,14 +17,12 @@ class TimeVariableElimination:
 
 
 class TimeBeliefPropagation:
+    timeout = 600
+
     def setup(self):
         self.alarm = get_example_model('alarm')
-        self.munin = get_example_model('munin')
 
     def time_query_alarm(self):
         infer = BeliefPropagation(self.alarm)
         infer.query(variables=['VENTLUNG', 'VENTALV', 'ARTCO2', 'CATECHOL'])
 
-    def time_query_munin(self):
-        infer = BeliefPropagation(self.munin)
-        infer.query(variables=['L_LNLC8_ADM_MALOSS', 'L_LNLLP_ADM_MALOSS', 'L_LNLC8_LP_ADM_MALOSS', 'L_LNLE_ADM_MALOSS'])
