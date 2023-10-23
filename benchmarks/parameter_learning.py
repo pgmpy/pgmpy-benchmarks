488b23634d0371ceea64c155dc539939d713bec4
Ankur Ankan <ankurankan@gmail.com>
2023-10-23 14:28:29 +0000

    Updates benchmarks

--- benchmarks/parameter_learning.py
+++ benchmarks/parameter_learning.py
@@ -17,10 +17,10 @@ class TimeMLEAlarm:
         return MaximumLikelihoodEstimator(alarm_model, alarm.simulate(int(1e3)))
 
     def time_mle(self, alarm_est):
-        alarm_est.get_parameters()
+        alarm_est.get_parameters(n_jobs=-1)
 
     def peakmem_mle(self, alarm_est):
-        alarm_est.get_parameters()
+        alarm_est.get_parameters(n_jobs=-1)
 
     def teardown(self, alarm_est):
         get_reusable_executor().shutdown(wait=True)
@@ -37,10 +37,10 @@ class TimeMLEMunin:
         return MaximumLikelihoodEstimator(munin_model, munin.simulate(int(1e3)))
 
     def time_mle(self, munin_est):
-        munin_est.get_parameters()
+        munin_est.get_parameters(n_jobs=-1)
 
     def peakmem_mle(self, munin_est):
-        munin_est.get_parameters()
+        munin_est.get_parameters(n_jobs=-1)
 
     def teardown(self, munin_est):
         get_reusable_executor().shutdown(wait=True)
@@ -51,18 +51,19 @@ class TimeBayesianEstimatorAlarm:
     repeat = 1
     number = 1
 
-    def setup(self):
+    def setup_cache(self):
         alarm = get_example_model('alarm')
         alarm_model = BayesianNetwork(alarm.edges())
         self.alarm_est = BayesianEstimator(alarm_model, alarm.simulate(int(1e3)))
+        return self.alarm_est
 
-    def time_bayesian_estimator(self):
-        self.alarm_est.get_parameters()
+    def time_bayesian_estimator(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def peakmem_bayesian_estimator(self):
-        self.alarm_est.get_parameters()
+    def peakmem_bayesian_estimator(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def teardown(self):
+    def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
 
 
@@ -71,18 +72,19 @@ class TimeBayesianEstimatorMunin:
     repeat = 1
     number = 1
 
-    def setup(self):
+    def setup_cache(self):
         munin = get_example_model('munin')
         munin_model = BayesianNetwork(munin.edges())
         self.munin_est = BayesianEstimator(munin_model, munin.simulate(int(1e3)))
+        return self.munin_est
 
-    def time_bayesian_estimator(self):
-        self.munin_est.get_parameters()
+    def time_bayesian_estimator(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def peakmem_bayesian_estimator(self):
-        self.munin_est.get_parameters()
+    def peakmem_bayesian_estimator(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def teardown(self):
+    def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
 
 
@@ -91,15 +93,19 @@ class TimeExpectationMaximizationAlarm:
     repeat = 1
     number = 1
 
-    def setup(self):
+    def setup_cache(self):
         alarm = get_example_model('alarm')
         alarm_model = BayesianNetwork(alarm.edges())
         self.alarm_est = ExpectationMaximization(alarm_model, alarm.simulate(int(1e3)))
+        return self.alarm_est
 
-    def time_em(self):
-        self.alarm_est.get_parameters()
+    def time_em(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def teardown(self):
+    def peakmem_em(self, est):
+        est.get_parameters(n_jobs=-1)
+
+    def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
 
 
@@ -108,13 +114,17 @@ class TimeExpectationMaximizationMunin:
     repeat = 1
     number = 1
 
-    def setup(self):
+    def setup_cache(self):
         munin = get_example_model('munin')
         munin_model = BayesianNetwork(munin.edges())
         self.munin_est = ExpectationMaximization(munin_model, munin.simulate(int(1e3)))
+        return self.munin_est
 
-    def time_em(self):
-        self.munin_est.get_parameters()
+    def time_em(self, est):
+        est.get_parameters(n_jobs=-1)
 
-    def teardown(self):
+    def peakmem_em(self, est):
+        est.get_parameters(n_jobs=-1)
+
+    def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
--- benchmarks/structure_learning.py
+++ benchmarks/structure_learning.py
@@ -19,20 +19,20 @@ class TimePCAlarm:
         return self.est
 
     def time_pc_stable(self, est):
-        est.estimate(variant='stable')
+        est.estimate(variant='stable', max_cond_vars=3)
 
     def time_pc_orig(self, est):
-        est.estimate(variant='orig')
+        est.estimate(variant='orig', max_cond_vars=3)
 
     def time_pc_parallel(self, est):
-        est.estimate(variant='parallel')
+        est.estimate(variant='parallel', max_cond_vars=3, n_jobs=-1)
 
     def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
 
 
 class TimePCMunin:
-    timeout = 1200.0
+    timeout = 600.0
     rounds = 1
     repeat = 1
     number = 1
@@ -43,14 +43,11 @@ class TimePCMunin:
         self.est = PC(samples)
         return self.est
 
-    def time_pc_stable(self, est):
-        est.estimate(variant='stable')
-
     def time_pc_orig(self, est):
-        est.estimate(variant='orig')
+        est.estimate(variant='orig', max_cond_vars=3)
 
     def time_pc_parallel(self, est):
-        est.estimate(variant='parallel')
+        est.estimate(variant='parallel', max_cond_vars=3, n_jobs=-1)
 
     def teardown(self, est):
         get_reusable_executor().shutdown(wait=True)
@@ -75,24 +72,24 @@ class TimeHillClimbAlarm:
         get_reusable_executor().shutdown(wait=True)
 
 
-class TimeHillClimbMunin:
-    timeout = 1200.0
-    rounds = 1
-    repeat = 1
-    number = 1
-
-    def setup(self):
-        model = get_example_model('munin1')
-        samples = model.simulate(n_samples=int(1e3), seed=42, show_progress=False)
-        self.scoring_method = K2Score(samples)
-        self.est = HillClimbSearch(data=samples)
-
-    def time_hillclimb(self):
-        self.est.estimate(max_indegree=4, scoring_method=self.scoring_method, max_iter=int(1e3))
-
-    def teardown(self):
-        get_reusable_executor().shutdown(wait=True)
-
+# class TimeHillClimbMunin:
+#     timeout = 2400.0
+#     rounds = 1
+#     repeat = 1
+#     number = 1
+# 
+#     def setup(self):
+#         model = get_example_model('munin1')
+#         samples = model.simulate(n_samples=int(1e3), seed=42, show_progress=False)
+#         self.scoring_method = K2Score(samples)
+#         self.est = HillClimbSearch(data=samples)
+# 
+#     def time_hillclimb(self):
+#         self.est.estimate(max_indegree=4, scoring_method=self.scoring_method, max_iter=int(1e2))
+# 
+#     def teardown(self):
+#         get_reusable_executor().shutdown(wait=True)
+# 
 
 class TimeTreeSearchAlarm:
     timeout = 1200.0
@@ -103,7 +100,7 @@ class TimeTreeSearchAlarm:
     def setup(self):
         model = get_example_model('alarm')
         samples = model.simulate(n_samples=int(1e3), show_progress=False)
-        self.est = TreeSearch(samples)
+        self.est = TreeSearch(samples, n_jobs=-1)
 
     def time_tan(self):
         self.est.estimate(estimator_type="tan", class_node="HISTORY", show_progress=False)
@@ -121,7 +118,7 @@ class TimeTreeSearchMunin:
     def setup(self):
         model = get_example_model('munin1')
         samples = model.simulate(n_samples=int(1e3), show_progress=False)
-        self.est = TreeSearch(samples)
+        self.est = TreeSearch(samples, n_jobs=-1)
 
     def time_tan(self):
         self.est.estimate(estimator_type="tan", class_node="R_LNLW_APB_MALOSS", show_progress=False)
